24acfae5aa526c620cf815d877137aebe4128060
VaughnVernon <vaughn@forcomprehension.com>
2020-06-16 13:16:29 -0700

    Moved to scooter.

--- src/main/java/io/vlingo/xoom/scooter/plugin/mailbox/blocking/BlockingMailbox.java
+++ src/main/java/io/vlingo/xoom/scooter/plugin/mailbox/blocking/BlockingMailbox.java
@@ -5,7 +5,7 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
-package io.vlingo.xoom.actors.plugin.mailbox.blocking;
+package io.vlingo.xoom.scooter.plugin.mailbox.blocking;
 
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/io/vlingo/xoom/scooter/plugin/mailbox/blocking/BlockingMailboxPlugin.java
+++ src/main/java/io/vlingo/xoom/scooter/plugin/mailbox/blocking/BlockingMailboxPlugin.java
@@ -5,7 +5,7 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
-package io.vlingo.xoom.actors.plugin.mailbox.blocking;
+package io.vlingo.xoom.scooter.plugin.mailbox.blocking;
 
 import io.vlingo.actors.Configuration;
 import io.vlingo.actors.Dispatcher;
