914ba79a1291089dd1e354bbd63d7b5cbf9bfba3
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-07-09 10:52:16 -0300

    Dependency trigger update

--- .travis/trigger_deps.sh
+++ .travis/trigger_deps.sh
@@ -41,7 +41,7 @@ MINOR=$(echo $VERSION | cut -f 2 -d '.')
 PATCH=$(echo $VERSION | cut -f 3 -d '.')
 NEW_VERSION=$MAJOR.$MINOR.$(($PATCH + 1))-SNAPSHOT
 
-for dependency in "vlingo-xoom-data";
+for dependency in "vlingo-schemata";
 do
     trigger_dependency $dependency $VERSION $NEW_VERSION
 done
