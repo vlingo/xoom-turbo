15fa95bad3f897073dbaca259ba78125da2ff68e
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-10-13 19:50:06 -0300

    Renabling auto-dispatch test classes

--- src/test/java/io/vlingo/xoom/annotation/autodispatch/DummyQueriesResource.java
+++ src/test/java/io/vlingo/xoom/annotation/autodispatch/DummyQueriesResource.java
@@ -7,14 +7,21 @@
 package io.vlingo.xoom.annotation.autodispatch;
 
 import io.vlingo.common.Completes;
+import io.vlingo.http.Method;
 import io.vlingo.http.Response;
+import io.vlingo.xoom.annotation.initializer.resources.DummyHandlers;
+import io.vlingo.xoom.annotation.persistence.DummyQueries;
+import io.vlingo.xoom.annotation.persistence.DummyQueriesActor;
 
-//@Queries(protocol = DummyQueries.class, actor = DummyQueriesActor.class)
-//@AutoDispatch(path = "/dummies", handlers = DummyHandlers.class)
+import static io.vlingo.xoom.annotation.initializer.resources.DummyHandlers.ADAPT_STATE;
+import static io.vlingo.xoom.annotation.initializer.resources.DummyHandlers.QUERY_ALL;
+
+@Queries(protocol = DummyQueries.class, actor = DummyQueriesActor.class)
+@AutoDispatch(path = "/dummies", handlers = DummyHandlers.class)
 public interface DummyQueriesResource {
 
-//    @Route(method = Method.GET, path = "any-path", handler = QUERY_ALL)
-//    @ResponseAdapter(handler = ADAPT_STATE)
+    @Route(method = Method.GET, path = "any-path", handler = QUERY_ALL)
+    @ResponseAdapter(handler = ADAPT_STATE)
     Completes<Response> dummyRouteForQueries();
 
 }
