e56ce0a021235e76b86f55b30b3325d49928ded7
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-10-23 12:57:52 +0200

    Fix cast to bools

--- src/main/java/io/vlingo/xoom/codegen/template/TemplateStandard.java
+++ src/main/java/io/vlingo/xoom/codegen/template/TemplateStandard.java
@@ -56,7 +56,7 @@ public enum TemplateStandard {
                 return Template.REST_RESOURCE_RETRIEVE_METHOD.filename;
             }
 
-            if(parameters.find(REQUIRE_ENTITY_LOADING)) {
+            if((boolean) parameters.find(REQUIRE_ENTITY_LOADING)) {
                 return Template.REST_RESOURCE_UPDATE_METHOD.filename;
             }
 
@@ -88,7 +88,7 @@ public enum TemplateStandard {
     XOOM_INITIALIZER(templateParameters -> Template.XOOM_INITIALIZER.filename,
             (name, parameters) -> "XoomInitializer"),
 
-    BOOTSTRAP(parameters -> parameters.find(USE_ANNOTATIONS) ?
+    BOOTSTRAP(parameters -> (boolean) parameters.find(USE_ANNOTATIONS) ?
             ANNOTATED_BOOTSTRAP.filename : DEFAULT_BOOTSTRAP.filename,
             (name, parameters) -> "Bootstrap"),
 
