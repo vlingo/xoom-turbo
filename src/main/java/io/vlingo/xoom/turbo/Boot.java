10ad50ae2a05568590fa6c7e87f2e97eab1eaa21
Petru Flueras <petru.flueras@gmail.com>
2022-07-23 15:28:01 +0300

    Adjustments based on latest changes of xoom-cluster (removed cluster nodes from configuration file)

--- src/main/java/io/vlingo/xoom/turbo/Boot.java
+++ src/main/java/io/vlingo/xoom/turbo/Boot.java
@@ -8,6 +8,8 @@
 package io.vlingo.xoom.turbo;
 
 import io.vlingo.xoom.actors.Configuration;
+import io.vlingo.xoom.cluster.StaticClusterConfiguration;
+import io.vlingo.xoom.cluster.model.NodeProperties;
 import io.vlingo.xoom.cluster.model.Properties;
 import io.vlingo.xoom.lattice.grid.Grid;
 import io.vlingo.xoom.lattice.grid.GridClusterProperties;
@@ -15,37 +17,40 @@ import io.vlingo.xoom.turbo.actors.Settings;
 
 public class Boot {
 
-  private static String resolvedNodeName;
+  private static String nodeProperties;
   private static Properties clusterProperties;
 
   public static Grid start(final String worldName) throws Exception {
     return start(worldName, null);
   }
 
-  public static Grid start(final String worldName, final String nodeName) throws Exception {
-    return start(worldName, nodeName, null);
+  public static Grid start(final String worldName, final String localNodeProperties) throws Exception {
+    return start(worldName, localNodeProperties, null);
   }
 
-  public static Grid start(final String worldName, final String nodeName, final Properties customProperties) throws Exception {
+  public static Grid start(final String worldName, final String localNodeProperties, final Properties customProperties) throws Exception {
     if (customProperties == null) {
       System.out.println("Unable to find xoom-cluster.properties. Using default grid cluster settings.");
-      clusterProperties = GridClusterProperties.oneNode();
+      StaticClusterConfiguration staticConfiguration = GridClusterProperties.oneNode();
+      clusterProperties = staticConfiguration.properties;
+      nodeProperties = staticConfiguration.propertiesOf(0);
     } else {
+      if (localNodeProperties == null || localNodeProperties.isEmpty()) {
+        // not allowed because 'cluster.seeds' have to correlate with 'localNodeProperties'
+        throw new IllegalArgumentException("'localNodeProperties' is mandatory when 'customProperties' is provided!");
+      }
+
+      // validate
+      NodeProperties.from(localNodeProperties);
+
       clusterProperties = customProperties;
+      nodeProperties = localNodeProperties;
     }
 
-    resolvedNodeName = resolveNodeName(nodeName);
-    return Grid.start(worldName, Configuration.define(), clusterProperties, resolvedNodeName);
+    return Grid.start(worldName, Configuration.define(), clusterProperties, nodeProperties);
   }
 
   public static int serverPort() {
     return Settings.serverPort();
-  }
-
-  private static String resolveNodeName(final String nodeName) {
-    if (nodeName == null || nodeName.isEmpty()) {
-      return clusterProperties.nodes().get(0);
-    }
-    return nodeName;
   }
 }
